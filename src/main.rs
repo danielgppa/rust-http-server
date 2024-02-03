a62d0528dde6fea085ab68648859af6d7e57d2c1
Daniel Andrade <danielgppa@gmail.com>
2024-02-03 17:43:35 +0000

    Add server and http modules

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,11 @@
+use server::Server;
+use http::Request;
+use http::Method;
+
+mod server;
+mod http;
+
 fn main() {
     let server = Server::new("localhost:8080".to_string());
     server.run();
-}
-
-struct Server {
-    addr: String,
-}
-
-impl Server {
-    fn new(addr: String) -> Self {
-        Self { addr }
-    }
-
-    fn run(self) {
-        println!("Listening on {}", self.addr);
-    }
-}
-
-struct Request {
-    path: String,
-    query_string: Option<String>,
-    method: Method,
-}
-
-enum Method {
-    GET,
-    DELETE,
-    POST,
-    PUT,
-    HEAD,
-    CONNECT,
-    OPTIONS,
-    TRACE,
-    PATCH,
 }
