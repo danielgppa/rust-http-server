c5f4f09880e6998757b24871b811f12897601af7
Daniel Andrade <danielgppa@gmail.com>
2024-02-07 19:21:02 +0000

    Added basic custom error messages

--- src/http/request.rs
+++ src/http/request.rs
@@ -1,5 +1,8 @@
 use super::method::Method;
 use std::convert::TryFrom;
+use std::error::Error;
+use std::fmt::{Debug, Display, Formatter, Result as FmtResult};
+
         pub struct Request {
             path: String,
             query_string: Option<String>,
@@ -11,12 +14,44 @@ use std::convert::TryFrom;
         }
 
         impl TryFrom<&[u8]> for Request {
-            type Error = String;
+            type Error = ParseError;
 
             fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
                 unimplemented!()
             }
         }
+
+        pub enum ParseError {
+            InvalidRequest,
+            InvalidEncoding,
+            InvalidProtocol,
+            InvalidMethod,
+        }
+
+        impl ParseError {
+            fn description(&self) -> &str {
+                match self {
+                    Self::InvalidRequest => "Invalid Request",
+                    Self::InvalidEncoding => "Invalid Encoding",
+                    Self::InvalidProtocol => "Invalid Protocol",
+                    Self::InvalidMethod => "Invalid Method",
+                }
+            }
+        }
+
+        impl Display for ParseError {
+            fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+                write!(f, "{}", self.description())
+            }
+        }
+
+        impl Debug for ParseError {
+            fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+                write!(f, "{}", self.description())
+            }
+        }
+
+        impl Error for ParseError {}
 
         trait Encrypt {
             fn encrypt(&self) -> Self {
