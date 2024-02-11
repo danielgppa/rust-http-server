973295e241931840926123707b85b1c0fd05936f
Daniel Andrade <danielgppa@gmail.com>
2024-02-11 20:21:12 +0000

    Added lifetimes to guarantee memory safety

--- src/http/request.rs
+++ src/http/request.rs
@@ -6,16 +6,16 @@ use std::convert::TryFrom;
 use std::error::Error;
 use std::fmt::{self, Debug, Display, Formatter, Result as FmtResult};
 
-        pub struct Request {
-            path: String,
-            query_string: Option<String>,
+        pub struct Request<'buf> {
+            path: &'buf str,
+            query_string: Option<&'buf str>,
             method: Method,
         }
 
-        impl TryFrom<&[u8]> for Request {
+        impl<'buf> TryFrom<&'buf [u8]> for Request<'buf> {
             type Error = ParseError;
 
-            fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
+            fn try_from(buf: &'buf [u8]) -> Result<Request<'buf>, Self::Error> {
                 let request = str::from_utf8(buf)?;
 
                 let (method, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
@@ -35,7 +35,11 @@ use std::fmt::{self, Debug, Display, Formatter, Result as FmtResult};
                     path = &path[..i];
                 }
 
-                unimplemented!()
+                Ok(Self {
+                    path,
+                    query_string,
+                    method,
+                })
             }
         }
 
