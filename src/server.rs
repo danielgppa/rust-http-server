f8115dc453dea98a4f303dbf92586bb2117a3309
Daniel Andrade <danielgppa@gmail.com>
2024-02-18 15:11:51 +0000

    Add send method to Response struct and update Server to handle requests and send responses

--- src/server.rs
+++ src/server.rs
@@ -1,6 +1,6 @@
-use std::io::Read;
+use std::io::{Read, Write};
 use std::result;
-use crate::http::Request;
+use crate::http::{Request, Response, StatusCode};
 use std::convert::TryFrom;
 use std::convert::TryInto;
 use std::net::TcpListener;
@@ -27,11 +27,19 @@ impl Server {
                         Ok(_) => {
                             println!("Received a request: {}", String::from_utf8_lossy(&buffer));
 
-                            match Request::try_from(&buffer[..]) {
+                            let response = match Request::try_from(&buffer[..]) {
                                 Ok(request) => {
                                     dbg!(request);
+                                    Response::new(StatusCode::Ok, Some("<h1>It works!</h1>".to_string()))
                                 },
-                                Err(e) => println!("Failed to parse a request: {}", e),
+                                Err(e) => {
+                                    println!("Failed to parse a request: {}", e);
+                                    Response::new(StatusCode::BadRequest, None)
+                                }
+                            };
+
+                            if let Err(e) = response.send(&mut stream) {
+                                println!("Failed to send response: {}", e);
                             }
                         }
                         Err(e) => println!("Failed to read from connection: {}", e),
