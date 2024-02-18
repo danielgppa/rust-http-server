d9ac1beff7b48f2a5ad7db9be992eb156ec4f2c0
Daniel Andrade <danielgppa@gmail.com>
2024-02-18 21:57:40 +0000

    Add WebsiteHandler and implement Handler trait

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 #![allow(dead_code)]
 
 use server::Server;
+use website_handler::WebsiteHandler;
 
 mod server;
 mod http;
+mod website_handler;
 
 fn main() {
     let server = Server::new("localhost:8080".to_string());
-    server.run();
+    server.run(WebsiteHandler);
 }
